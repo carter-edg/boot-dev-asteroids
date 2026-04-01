f1d9b20d45bd2f54f9a304532dfe7ae3bfa60cd5
Carter Edginton <249395916+carter-edg@users.noreply.github.com>
2026-04-01 00:52:16 -0600

    shooting

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from player import Player
 from asteroid import Asteroid
 from logger import log_state, log_event
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -15,10 +16,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     field = AsteroidField()
     clock = pygame.time.Clock()
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from shot import Shot
 import pygame
 
 class Player(CircleShape):
@@ -39,4 +40,13 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        direction = pygame.Vector2(0,1)
+        direction = direction.rotate(self.rotation)
+        direction *= PLAYER_SHOOT_SPEED
+        shot.velocity = direction
 
