79aa270a450f3d87562cf6f52c4777ca2591dabb
Carter Edginton <249395916+carter-edg@users.noreply.github.com>
2026-03-31 22:59:06 -0600

    circleshape

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -14,6 +18,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) /1000
 
 if __name__ == "__main__":
     main()
