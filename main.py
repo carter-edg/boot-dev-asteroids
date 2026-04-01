513e0fa762cfb8d6ab2fd2237b895b2188b18e8f
Carter Edginton <249395916+carter-edg@users.noreply.github.com>
2026-04-01 00:27:53 -0600

    circle collision

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
+from asteroid import Asteroid
 from logger import log_state
+from asteroidfield import AsteroidField
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -11,9 +14,13 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
+    field = AsteroidField()
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
